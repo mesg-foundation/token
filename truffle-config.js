4684d140d052ff587b815ddcb175cefb5bcd0935
Nicolas Mahe <nicolas@mahe.me>
2019-01-19 12:08:03 +0700

    add mainnet to truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -71,6 +71,17 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
+    mainnet: {
+      provider: () => new HDWalletProvider(privateKey, 'https://mainnet.infura.io/v3/' + infuraKey),
+      network_id: 1, // Ropsten's id
+      gas: 8000000, // Ropsten has a lower block limit than mainnet
+      gasPrice: 20000000000, // 20 gwei (in wei) (default: 100 gwei)
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+      production: true
+    },
+
     ropsten: {
       provider: () => new HDWalletProvider(privateKey, 'https://ropsten.infura.io/v3/' + infuraKey),
       network_id: 3, // Ropsten's id
